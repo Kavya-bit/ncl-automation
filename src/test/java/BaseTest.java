c6985ff600865564f4744d849a99f090b39636a3
kavya-bit <kavyaqa.11@gmail.com>
2020-10-22 11:27:19 -0400

    Decreased wait time

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -13,7 +13,7 @@ public class BaseTest {
         System.setProperty("webdriver.chrome.driver", driverPath);
         /*Create driver object for CHROME browser*/
         this.driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(12, TimeUnit.SECONDS);
 
     }
 }
